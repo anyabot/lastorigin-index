48659abc2e2a5e4de599b783501d0da8d24cd475
anyabot <52899201+anyabot@users.noreply.github.com>
2020-03-14 12:38:07 +0900

    Update main.7c5dd7d4.chunk.js

--- static/js/main.7c5dd7d4.chunk.js
+++ static/js/main.7c5dd7d4.chunk.js
@@ -62,9 +62,9 @@
                             ContainerStyle: null
                         }, a.NameList = null, a.isResultClosing = !1, a.isSearchResultClosed = !0, a.updateSearchList = function(e, t) {
                             if (e.showSearchResult = !0, e.showIndexResult = !1, t.length > 0) {
-                                var n = new x.Searcher(t),
+                                var n = new x.Searcher(t.toLowerCase()),
                                     l = Object.keys(a.NameList).reduce((function(e, t) {
-                                        return -1 !== n.search(new RegExp(t, "i")) && e.push([t, n.search(new RegExp(t, "i"))]), e
+                                        return -1 !== n.search(t.toLowerCase()) && e.push([t, n.search(t.toLowerCase())]), e
                                     }), []).sort((function(e, t) {
                                         return e[1] < t[1] ? -1 : e[1] > t[1] ? 1 : e[0] < t[0] ? -1 : e[0] > t[0] ? 1 : 0
                                     })).map((function(e) {
